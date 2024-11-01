e1934844243513e4870c3ce299349359aaf1c68e
Luizfmmiranda8 <126125049+Luizfmmiranda8@users.noreply.github.com>
2024-11-01 15:37:50 +0000

    Declaração da posição dos navios e demonstração em um tabuleiro 10x10

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,14 +1,61 @@
 #include <stdio.h>
 
 // Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    //Declaração do tabuleiro e declaração e iniciação dos navios
+    int tabuleiro[10][10];
+    int navio1[3][2] = 
+    {
+        {1,1}, 
+        {2,1}, 
+        {3,1}
+    };
+    int navio2[3][2] = 
+    {
+        {3,5}, 
+        {3,6}, 
+        {3,7}
+    };
+
+    for(int i = 0; i < 10; i++)
+    {
+        for(int j = 0; j < 10; j++)
+        {
+            if(i == navio1[0][0] && j == navio1[0][1])
+            {
+                printf("3");
+            }
+            else if(i == navio1[1][0] && j == navio1[1][1])
+            {
+                printf("3");
+            }
+            else if(i == navio1[2][0] && j == navio1[2][1])
+            {
+                printf("3");
+            }
+            if(i == navio2[0][0] && j == navio2[0][1])
+            {
+                printf("3");
+            }
+            else if(i == navio2[1][0] && j == navio2[1][1])
+            {
+                printf("3");
+            }
+            else if(i == navio2[2][0] && j == navio2[2][1])
+            {
+                printf("3");
+            }
+            else
+            {
+                printf("0");
+            }
+        }
+        printf("\n");
+    }
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
