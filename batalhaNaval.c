9de2eafd621d3282df4386e93b1e70a656056db1
Luizfmmiranda8 <126125049+Luizfmmiranda8@users.noreply.github.com>
2024-11-01 16:36:02 +0000

    Adição de 2 navios na diagonal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,9 +3,7 @@
 // Desafio Batalha Naval - MateCheck
 int main() {
     // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+
     //Declaração do tabuleiro e declaração e iniciação dos navios
     int tabuleiro[10][10];
     int navio1[3][2] = 
@@ -20,47 +18,79 @@ int main() {
         {3,6}, 
         {3,7}
     };
+    int navio3[3][2] = 
+    {
+        {4,3}, 
+        {5,2}, 
+        {6,1}
+    };
+    int navio4[3][2] = 
+    {
+        {7,6}, 
+        {8,7}, 
+        {9,8}
+    };
 
+    //Percorrendo o tabuleiro e verificando se coincide com a posição de algum navio
     for(int i = 0; i < 10; i++)
     {
         for(int j = 0; j < 10; j++)
         {
             if(i == navio1[0][0] && j == navio1[0][1])
             {
-                printf("3");
+                printf("3 ");
             }
             else if(i == navio1[1][0] && j == navio1[1][1])
             {
-                printf("3");
+                printf("3 ");
             }
             else if(i == navio1[2][0] && j == navio1[2][1])
             {
-                printf("3");
+                printf("3 ");
             }
-            if(i == navio2[0][0] && j == navio2[0][1])
+            else if(i == navio2[0][0] && j == navio2[0][1])
             {
-                printf("3");
+                printf("3 ");
             }
             else if(i == navio2[1][0] && j == navio2[1][1])
             {
-                printf("3");
+                printf("3 ");
             }
             else if(i == navio2[2][0] && j == navio2[2][1])
             {
-                printf("3");
+                printf("3 ");
+            }
+            else if(i == navio3[0][0] && j == navio3[0][1])
+            {
+                printf("3 ");
+            }
+            else if(i == navio3[1][0] && j == navio3[1][1])
+            {
+                printf("3 ");
+            }
+            else if(i == navio3[2][0] && j == navio3[2][1])
+            {
+                printf("3 ");
+            }
+            else if(i == navio4[0][0] && j == navio4[0][1])
+            {
+                printf("3 ");
+            }
+            else if(i == navio4[1][0] && j == navio4[1][1])
+            {
+                printf("3 ");
+            }
+            else if(i == navio4[2][0] && j == navio4[2][1])
+            {
+                printf("3 ");
             }
             else
             {
-                printf("0");
+                printf("0 ");
             }
         }
         printf("\n");
     }
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
